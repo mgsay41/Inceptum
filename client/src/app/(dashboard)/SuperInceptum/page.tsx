a216451b4be1125b693fc1b30d989e02fb6d2c99
Mohamed Gamal <mgsay41@gmail.com>
2024-11-22 15:56:37 +0200

    admin dashboard

--- client/src/app/(dashboard)/SuperInceptum/page.tsx
+++ client/src/app/(dashboard)/SuperInceptum/page.tsx
@@ -1,16 +1,62 @@
 import UserCard from "@/components/UserCard";
 import React from "react";
 
+import {
+  Bell,
+  DollarSign,
+  Users,
+  BookOpen,
+  Briefcase,
+  GraduationCap,
+  UserCog,
+  HeartHandshake,
+} from "lucide-react";
+
+const colors = {
+  blue: "#3b82f6",
+  green: "#10b981",
+  yellow: "#f59e0b",
+  purple: "#8b5cf6",
+  pink: "#ec4899",
+  indigo: "#6366f1",
+  red: "#ef4444",
+  orange: "#f97316",
+};
+
 const PageSuperInceptum = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       <div className="w-full ">
         {/* USER CARD */}
-        <div className="flex gap-4 justify-between">
-          <UserCard type="student" />
-          <UserCard type="Instructor" />
-          <UserCard type="Assistant" />
-          <UserCard type="Course" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+          <UserCard
+            icon={<Users />}
+            title="Students"
+            value="1,000"
+            subtext="80 new this month"
+            color={colors.blue}
+          />
+          <UserCard
+            icon={<UserCog />}
+            title="Trainers"
+            value="200"
+            subtext="15 new this month"
+            color={colors.green}
+          />
+          <UserCard
+            icon={<HeartHandshake />}
+            title="Assistants"
+            value="34"
+            subtext="5 new this month"
+            color={colors.yellow}
+          />
+          <UserCard
+            icon={<BookOpen />}
+            title="Courses"
+            value="50"
+            subtext="Active: 30, Upcoming: 20"
+            color={colors.purple}
+          />
         </div>
       </div>
     </div>
--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const UserCard = ({ type }: { type: string }) => {
-  return <div>UserCard</div>;
+type UserCardProps = {
+  icon: React.ReactNode;
+  title: string;
+  value: string | number;
+  subtext: string;
+  color: string;
+};
+
+const UserCard = ({ icon, title, value, subtext, color }: UserCardProps) => {
+  return (
+    <div
+      className="bg-white rounded-lg p-6 shadow-lg border-t-4"
+      style={{ borderColor: color }}
+    >
+      <div className="flex items-center mb-2">
+        <div className="mr-3" style={{ color }}>
+          {icon}
+        </div>
+        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+      </div>
+      <p className="text-3xl font-bold mb-1 text-gray-800">{value}</p>
+      <p className="text-sm text-gray-600">{subtext}</p>
+    </div>
+  );
 };
 
 export default UserCard;
